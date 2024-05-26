3c988f339b76800006660375391e9904dad51d44
Aleksandr Petrov <aleksandr.petrov@info-pro.ru>
2024-05-26 08:11:42 +0200

    init

---
+++ apps/inventory/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { ProductListComponent } from '@nx-test/products';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule, ProductListComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'inventory';
+}
